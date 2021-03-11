0af0748faac2f21e850df3c872b6d1c5be479e21
Ivan Gartsev <code@zentralweb.de>
2021-03-11 14:27:17 +0100

    Update swprep.sh

--- swprep.sh
+++ swprep.sh
@@ -9,5 +9,4 @@ sudo apt install -y mysql-server apache2
 sudo apt install -y composer nodejs npm chromium-browser default-jre-headless git
 sudo apt install -y php-gd php-intl php-iconv php-mbstring php-mysql php-xml php-zip php-json
 sudo apt-get update
-sudo apt-get install php-intl php-xml php-zip php-apcu php-mbstring php-json php-gd unzip nodejs openssh-server git php-curl curl npm
-sudo npm install -g npm@latest
+sudo apt-get install php-intl php-xml php-zip php-apcu php-mbstring php-json php-gd unzip nodejs openssh-server git php-curl curl
